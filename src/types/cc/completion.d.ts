ab68b7010884582ca9f964c8d4ab49508a52b5e4
Chandler Ferry <chandlerferry@gmail.com>
2025-02-13 18:20:35 -0800

    chore: prettier

--- src/types/cc/completion.d.ts
+++ src/types/cc/completion.d.ts
@@ -1,9 +1,13 @@
 /** @noSelfInFile **/
 /** @noResolution **/
-declare module "cc.completion" {
-    export function choice(text: string, choices: string[], add_space?: boolean): string[];
-    export function peripheral(text: string, add_space?: boolean): string[];
-    export function side(text: string, add_space?: boolean): string[];
-    export function setting(text: string, add_space?: boolean): string[];
-    export function command(text: string, add_space?: boolean): string[];
+declare module 'cc.completion' {
+	export function choice(
+		text: string,
+		choices: string[],
+		add_space?: boolean,
+	): string[]
+	export function peripheral(text: string, add_space?: boolean): string[]
+	export function side(text: string, add_space?: boolean): string[]
+	export function setting(text: string, add_space?: boolean): string[]
+	export function command(text: string, add_space?: boolean): string[]
 }
--- src/utils/yield.ts
+++ src/utils/yield.ts
@@ -1,9 +1,8 @@
-
 let yieldTime = os.clock()
-export const yieldSmart =  () => {
-    if (os.clock() - yieldTime > 2) {
-        os.queueEvent("yield")
-        os.pullEvent()
-        yieldTime = os.clock()
-    }
+export const yieldSmart = () => {
+	if (os.clock() - yieldTime > 2) {
+		os.queueEvent('yield')
+		os.pullEvent()
+		yieldTime = os.clock()
+	}
 }
